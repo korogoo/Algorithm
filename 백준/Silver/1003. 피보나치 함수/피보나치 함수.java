79df749750195e3c3b38604c97b1510647bbb23f
korogoo <zzgg3040@naver.com>
2026-03-29 03:40:09 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,25 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    int t = Integer.parseInt(br.readLine());
 
-    int[] arr = new int[41];
-    arr[1] = 1;
-    for (int i = 2; i < arr.length; i++) {
-      arr[i] = arr[i - 1] + arr[i - 2];
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    for (int i = 0; i < t; i++) {
-      int x = Integer.parseInt(br.readLine());
-      if (x == 0) {
-        sb.append("1 0\n");
-        continue;
-      }
-      sb.append(arr[x - 1]).append(" ").append(arr[x]).append("\n");
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[41][2];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+
+        System.out.print(sb);
     }
-    System.out.println(sb);
-  }
 }
